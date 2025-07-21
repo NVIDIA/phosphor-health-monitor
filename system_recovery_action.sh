6681c05f5d82fe49f14343de8a2eef7a42d1a60e
Chandramohan Harkude <charkude@nvidia.com>
2025-07-21 12:18:18 -0700

    Meson option to enable/disable event logging
    Added meson option to enable or disable the event logging from code
    
    Testing:
    
    ```
    
    ASSERT message logged in journal when CPU usage crosses the threshold
    
    Jul 21 15:41:38 gb200nvl-bmc health-monitor[1425]: ASSERT: Health Metric CPU crossed xyz.openbmc_project.Common.Threshold.Type.Critical upper threshold 98.000000 and current value is 100.000000
    Jul 21 15:41:38 gb200nvl-bmc health-monitor[1425]: Starting systemd unit HMSystemRecovery@.service with resource CPU
    Jul 21 15:41:39 gb200nvl-bmc health-monitor[1425]: ASSERT: Health Metric CPU crossed xyz.openbmc_project.Common.Threshold.Type.Warning upper threshold 90.000000 and current value is 100.000000
    Jul 21 15:41:39 gb200nvl-bmc health-monitor[1425]: Starting systemd unit HMSystemWarning@.service with resource CPU
    Jul 21 15:41:40 gb200nvl-bmc health-monitor[1425]: ASSERT: Health Metric CPU_User crossed xyz.openbmc_project.Common.Threshold.Type.Warning upper threshold 90.000000 and current value is 94.754761
    Jul 21 15:41:40 gb200nvl-bmc health-monitor[1425]: Starting systemd unit HMSystemWarning@.service with resource CPU_User
    Jul 21 15:43:06 gb200nvl-bmc health-monitor[1425]: DEASSERT: Health Metric CPU_User is below xyz.openbmc_project.Common.Threshold.Type.Warning upper threshold
    Jul 21 15:47:07 gb200nvl-bmc health-monitor[1425]: ASSERT: Health Metric CPU_User crossed xyz.openbmc_project.Common.Threshold.Type.Warning upper threshold 90.000000 and current value is 90.178455
    
    no events logged
    
    root@gb200nvl-bmc:~# curl -k -u root:0penBmc https://127.0.0.1/redfish/v1/Managers/BMC_0/LogServices/EventLog/Entries
    {
      "@odata.id": "/redfish/v1/Managers/BMC_0/LogServices/EventLog/Entries",
      "@odata.type": "#LogEntryCollection.LogEntryCollection",
      "Description": "Collection of System Event Log Entries",
      "Members": [],
      "Members@odata.count": 0,
      "Name": "Manager Event Log Entries"
    }
    
    ```
    
    Fixes nvbug https://nvbugspro.nvidia.com/bug/5200368

--- system_recovery_action.sh
+++ system_recovery_action.sh
@@ -16,7 +16,7 @@ if [[ "$RESOURCE" == "Storage_"* ]]; then
         if  mountpoint $STORAGEPATH &> /dev/null;
             then
             echo "Critical threshold hit for $RESOURCE  resource on $STORAGEPATH hence erasing the mtd partition to recover the storage"
-            mtdnum=$(mount -l | awk -v path="$STORAGEPATH" '$0 ~ path {split($1, arr, "/"); gsub("mtdblock", "", arr[3]); print arr[3]}')
+               # mtdnum=$(mount -l | awk -v path="$STORAGEPATH" '$0 ~ path {split($1, arr, "/"); gsub("mtdblock", "", arr[3]); print arr[3]}')
                # echo "Erasing /dev/mtd$mtdnum partition "
                # systemctl stop xyz.openbmc_project.Logging.service
                #systemctl stop xyz.openbmc_project.Dump.Manager.service
@@ -26,15 +26,15 @@ if [[ "$RESOURCE" == "Storage_"* ]]; then
         echo "Critical threshold hit for $RESOURCE  resource on $STORAGEPATH hence doing factory reset to recover the storage"
         #fw_setenv openbmconce factory-reset
     fi  
-    busctl call xyz.openbmc_project.Logging /xyz/openbmc_project/logging xyz.openbmc_project.Logging.Create Create ssa{ss} "OpenBMC.0.4.BMCSystemResourceInfo" "xyz.openbmc_project.Logging.Entry.Level.Informational" 4 "REDFISH_MESSAGE_ID" "OpenBMC.0.4.BMCSystemResourceInfo" "REDFISH_MESSAGE_ARGS" " $RESOURCE, $STORAGEPATH" xyz.openbmc_project.Logging.Entry.Resolution "None" "namespace" "Manager"
+#    busctl call xyz.openbmc_project.Logging /xyz/openbmc_project/logging xyz.openbmc_project.Logging.Create Create ssa{ss} "OpenBMC.0.4.BMCSystemResourceInfo" "xyz.openbmc_project.Logging.Entry.Level.Informational" 4 "REDFISH_MESSAGE_ID" "OpenBMC.0.4.BMCSystemResourceInfo" "REDFISH_MESSAGE_ARGS" " $RESOURCE, $STORAGEPATH" xyz.openbmc_project.Logging.Entry.Resolution "None" "namespace" "Manager"
 #    sleep 5
 #    systemctl start reboot.target
 elif [[ "$RESOURCE" == "EMMC_"* ]]; then
     echo "Critical threshold hit for resource $RESOURCE: usage = $USAGE "
-    busctl call xyz.openbmc_project.Logging /xyz/openbmc_project/logging xyz.openbmc_project.Logging.Create Create ssa{ss} "OpenBMC.0.4.BMCSystemResourceInfo" "xyz.openbmc_project.Logging.Entry.Level.Informational" 4 "REDFISH_MESSAGE_ID" "OpenBMC.0.4.BMCSystemResourceInfo" "REDFISH_MESSAGE_ARGS" " $RESOURCE, $USAGE" xyz.openbmc_project.Logging.Entry.Resolution "None" "namespace" "Manager"
+#    busctl call xyz.openbmc_project.Logging /xyz/openbmc_project/logging xyz.openbmc_project.Logging.Create Create ssa{ss} "OpenBMC.0.4.BMCSystemResourceInfo" "xyz.openbmc_project.Logging.Entry.Level.Informational" 4 "REDFISH_MESSAGE_ID" "OpenBMC.0.4.BMCSystemResourceInfo" "REDFISH_MESSAGE_ARGS" " $RESOURCE, $USAGE" xyz.openbmc_project.Logging.Entry.Resolution "None" "namespace" "Manager"
 else
     echo "Critical threshold hit for $RESOURCE resource "
     TOPOUTPUT=$(/bin/bash /usr/bin/run_top.sh $RESOURCE)
     echo "TOPOUTPUT: $TOPOUTPUT"
-    busctl call xyz.openbmc_project.Logging /xyz/openbmc_project/logging xyz.openbmc_project.Logging.Create Create ssa{ss} "OpenBMC.0.4.BMCSystemResourceInfo" "xyz.openbmc_project.Logging.Entry.Level.Informational" 4 "REDFISH_MESSAGE_ID" "OpenBMC.0.4.BMCSystemResourceInfo" "REDFISH_MESSAGE_ARGS" " $RESOURCE, $TOPOUTPUT" xyz.openbmc_project.Logging.Entry.Resolution "None" "namespace" "Manager"
+#    busctl call xyz.openbmc_project.Logging /xyz/openbmc_project/logging xyz.openbmc_project.Logging.Create Create ssa{ss} "OpenBMC.0.4.BMCSystemResourceInfo" "xyz.openbmc_project.Logging.Entry.Level.Informational" 4 "REDFISH_MESSAGE_ID" "OpenBMC.0.4.BMCSystemResourceInfo" "REDFISH_MESSAGE_ARGS" " $RESOURCE, $TOPOUTPUT" xyz.openbmc_project.Logging.Entry.Resolution "None" "namespace" "Manager"
 fi                                      
